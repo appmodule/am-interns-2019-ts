b9ccef46a9c2bd2766e9636fb605752934907063
Dragana <dragana.stojnev@elfak.rs>
2019-07-17 15:00:55 +0200

    new swagger api

--- TimeShift/api/controllers/channel.js
+++ TimeShift/api/controllers/channel.js
@@ -1,29 +1,4 @@
-//'use strict';
-/*
- 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-*/
 
-/*
- Modules make it possible to import JavaScript files into your application.  Modules are imported
- using 'require' statements that give you a reference to the module.
-
-  It is a good idea to list the modules that your application depends on in the package.json in the project root
- */
-//var util = require('util');
-
-/*
- Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
-
- For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
-
- Either:
-  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
-  - Or the operationId associated with the operation in your Swagger document
-
-  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
-  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
- */
 module.exports = {
   getChannel: getChannel,
   addChannel: addChannel,
@@ -31,40 +6,68 @@ module.exports = {
   
 };
 
-/*
-  Functions in a127 controllers used for operations should take two parameters:
-
-  Param 1: a handle to the request object
-  Param 2: a handle to the response object
- */
-/*function addChannel(req, res) {
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
-
-  // this sends back a JSON response which is a single string
-  res.json(hello);
-}*/
-
 function getChannel(req, res1)
 {
-    // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
     var id = req.swagger.params.id.value || 'stranger';
-    //var hello = util.format('Hello, %s!', id);
-  
-    const db = require('../database')
 
+    const db = require('../database')
+    const fs = require('fs')
+    fs.appendFile('./playlist.txt', '#EXTM3U\r\n', function (err) {
+      if (err) throw err;
+    });
+    
+        let variant = null;
+    
         db.query('SELECT * FROM channels WHERE id = $1', [id], (err, res) => {
-            if (err) {
+            if (err) 
+            {
             return err
             }
-            res1.json(res.rows[0])
+            var channel = res.rows[0];
+
+            db.query('SELECT * FROM variants WHERE channel_id = $1',[channel.id],(err,res)=>
+            {
+              if (err)
+              {
+                return err
+              }
+              variant = res.rows[0];              
+            })
+
+            let variantId = res.rows[0].id;// kako ovde da korisnik odabere
+
+            fs.appendFile('./playlist.txt', '#EXT-X-VERSION:'+variantId+"\r\n", function (err)
+            {
+              if (err) throw err;
+            });
+            
+            db.query('SELECT * FROM saved_chunks WHERE variant_id = $1 order by timestamp asc',[variantId],(err,res)=>
+            {
+              if (err)
+              {
+                return err
+              }
+              var chunks = res.rows
+              res1.json(res.rows)
+              const fs = require('fs')
+
+              chunks.forEach(element => 
+              {
+                let toAppend = "#EXT-X-STREAM-INF:BANDWIDTH="+variant.bandwidth+",CODECS='"+variant.codecs+"'"+element.filepath+"\r\n";
+                fs.appendFile('./playlist.txt', toAppend, function (err) 
+                {
+                  if (err) throw err;
+                });
+                
+              //res1.end('./playlist.txt');
+              });
+              
+            })
         })
-    //res.json(hello);
   }
 
-  function addChannel(req,res1)
-  {
+  function addChannel(req,res1){
+
     var uri = req.swagger.params.uri.value;
     var number_failed = req.swagger.params.number_failed.value;
     var number_succeded = req.swagger.params.number_succeded.value;
