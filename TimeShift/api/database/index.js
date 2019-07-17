9a0e8c51f328b0627ab4e60040ad082051ab0d44
Dragana <dragana.stojnev@elfak.rs>
2019-07-17 11:27:00 +0200

    channels swagger api

--- TimeShift/api/database/index.js
+++ TimeShift/api/database/index.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg')
-
-const pool = new Pool()
+const env = process.env
+const pool = new Pool({
+  user: env.PGUSER,
+  host: env.PGHOSTADDR,
+  database: env.PGDATABASE,
+  password: env.PGPASSWORD,
+  port: env.PGPORT,
+})
 
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback)
   }
 }
-
