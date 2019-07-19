c5a93c9f58ef0daee62f76cf4d20b92d51aab9de
Dusan Zivanovic <zdule1998@gmail.com>
2019-07-19 10:34:58 +0200

    Added variant model

---
+++ TimeShift/models/variant.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const variant = sequelize.define('variant', {
+    channel_id: DataTypes.INTEGER,
+    uri: DataTypes.TEXT,
+    codecs: DataTypes.TEXT,
+    bandwidth: DataTypes.INTEGER,
+    disabled: DataTypes.BOOLEAN
+  }, {});
+  variant.associate = function(models) {
+    variant.belongsTo(models.channel)
+  };
+  return variant;
+};
