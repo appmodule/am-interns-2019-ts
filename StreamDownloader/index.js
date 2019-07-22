e441f669862a05d0bdab3b7e5e87f2dcf16aa82b
Dusan Zivanovic <zdule1998@gmail.com>
2019-07-22 10:59:44 +0200

    Make master playlist download asynchronous

--- StreamDownloader/index.js
+++ StreamDownloader/index.js
@@ -1,42 +1,16 @@
 'use strict';
 require('dotenv').config()
 
-//const fs = require('fs')
-// Create a readable stream from a URL
-//const strone = "http://streams.appmodule.net/output5/rtsun_hd/track_0_900/playlist.m3u8"
-//const strtwo = "http://streams.appmodule.net/output5/rtsun_hd/playlist.m3u8"
-
 const getChannels = require('./database/channels.js').getChannels
-const variants = require('./database/variants.js')
-const getVariants = require('./hls/index.js').getVariants
-const {VariantDownloader} = require('./hls/VariantDownloader.js')
-const ensureExistsDir = require('./util.js').ensureExistsDir
+const channelDownloader = require('./hls/ChannelDownloader.js')
 
 async function main() {
-    if (!process.env.FILE_SERVER) {
-        throw "NO FILESERVER VARIABLE"
-    }
 
     const channels = await getChannels()
 
-    let vars = []
     for (let channel of channels) {
-        vars = vars.concat(await getVariants(channel.uri, channel.id))
+        channelDownloader(channel.uri, channel.id)
     }
-
-    vars = await variants.linkToDatabase(vars)
-    console.log(vars)
-
-    ensureExistsDir("files")
-    let downloaders = []
-    for (let variant of vars) {
-        downloaders.push(new VariantDownloader(variant))
-    }
-
 }
 
 main()
-
-
-
-
