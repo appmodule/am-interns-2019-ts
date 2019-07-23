9d152bd0afa258bf90043236d1bf780bb4557633
Dusan Zivanovic <zdule1998@gmail.com>
2019-07-23 09:46:13 +0200

    Update variants to use sequelize, StreamDownloader

--- StreamDownloader/database/variants.js
+++ StreamDownloader/database/variants.js
@@ -1,20 +1,18 @@
 'use strict';
 
-const query = require('./query.js').query
+const variant = require('../../TimeShift/models').variant
 
 async function linkToDatabase(variants) {
-    const search_query = "SELECT id from variants where \
-                                 channel_id = $1 and bandwidth=$2 and codecs=$3 and uri=$4;"
-    const add_query = "INSERT INTO variants (channel_id, bandwidth, codecs, uri) \
-                              VALUES ($1, $2, $3, $4) RETURNING id"
-    for (let variant of variants) {
-        const vals = [variant.channel_id, variant.bandwidth, variant.codecs, variant.uri]
-        const rows = (await query(search_query, vals)).rows
-        if (rows.length == 0) {
-            variant.id = (await query(add_query, vals)).rows[0].id
-        } else {
-            variant.id = rows[0].id
-        }
+    for (let v of variants) {
+        let res = await variant.findOrCreate({
+            where : {
+                channel_id : v.channel_id,
+                bandwidth : v.bandwidth,
+                codecs : v.codecs,
+                uri : v.uri,
+            }
+        })
+        v.id = res[0].id
     }
     return variants
 }
